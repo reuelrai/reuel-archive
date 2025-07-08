8cfe839b6581f4db2a87927800ccdf5c583abf89
reuel.rai <149403520+reuelrai@users.noreply.github.com>
2025-07-08 12:34:13 +0545

    Update dreams.tsx

--- src/app/dreams/dreams.tsx
+++ src/app/dreams/dreams.tsx
@@ -10,7 +10,7 @@ export const dreams = [
     {
         id: 2,
         title: "The accident",
-        content: `Today in one of my dreams. I had forgotten my plane ticket right at the airport and I was going home to get my ticket as I was going home in a rush. I saw a girl getting into an accident right near the exit of the airport. It seemed like she had been hit my a car and was lying on the road. I raised my hand stopped and incoming traffic and helped the girl. Though it wasnt too severe but her leg taken taken some damage and I took care of her and later she came to my house and we spent few moments together. She had round face, light skin colour and her eyes were beautiful.`,
+        content: `Today, in one of my dreams, I had forgotten my plane ticket right at the airport. As I was rushing home to get it, I saw a girl getting into an accident near the exit of the airport. It seemed like she had been hit by a car and was lying on the road. I raised my hand, stopped the incoming traffic, and helped her. Though it wasn’t too severe, her leg had taken some damage. I took care of her, and later she came to my house. We spent a few moments together. She had a round face, light skin, and beautiful eyes.`,
         date: "2025-07-08",
     },
     
